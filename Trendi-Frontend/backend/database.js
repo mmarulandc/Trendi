b1170abdc02cf94a45a0e17d517b485a6ce3a8e4
Mateo Marulanda Cifuentes <mateo.marulanda@arkix.com>
2019-09-17 16:56:06 -0500

    test

--- Trendi-Frontend/backend/database.js
+++ Trendi-Frontend/backend/database.js
@@ -1,6 +1,6 @@
 const db = require("mongoose");
 
-db.connect("mongodb://localhost:27017/trendi",{useNewUrlParser:true})
+db.connect("mongodb://mongo:27017/trendi",{useNewUrlParser:true})
     .then(database =>  console.log("db conected"))
     .catch(err =>  console.log(err) );
 
